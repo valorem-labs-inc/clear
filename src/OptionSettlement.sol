103791d7f06381eab3986b6710e7e7c31fd803de
Alcibiades Athens <alcibiades.eth@protonmail.com>
2022-03-09 16:07:01 -0500

    document structs

--- src/OptionSettlement.sol
+++ src/OptionSettlement.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: BUSL 1.1
 pragma solidity 0.8.11;
 
+import "base64/Base64.sol";
 import "solmate/tokens/ERC1155.sol";
 
 /**
@@ -26,22 +27,31 @@ enum Type {
 }
 
 struct Option {
-    // Random seed created at the time of option chain creation
-    uint256 settlementSeed;
     // The underlying asset to be received
     address underlyingAsset;
-    //
+    // The address of the asset needed for exercise
     address exerciseAsset;
+    // Random seed created at the time of option chain creation
+    uint256 settlementSeed;
+    // The amount of the underlying asset contained within an option contract of this type
+    uint256 underlyingAmount;
+    // The amount of the exercise asset required to exercise this option
+    uint256 exerciseAmount;
+    // The timestamp after which this option may be exercised
     uint256 exerciseTimestamp;
+    // The timestamp before which this option must be exercised
     uint256 expiryTimestamp;
 }
 
 struct Claim {
-    // TODO(State about asset exercise, etc)
+    // Which option was written
     uint256 option;
-    // These are 1:1 contracts with the underlying struct
+    // These are 1:1 contracts with the underlying Option struct
+    // The number of contracts written in this claim
     uint256 amountWritten;
+    // The amount of contracts assigned for exercise to this claim
     uint256 amountExercised;
+    // The two amounts above along with the option info, can be used to calculate the underlying assets
 }
 
 contract OptionSettlementEngine is ERC1155 {
@@ -56,6 +66,8 @@ contract OptionSettlementEngine is ERC1155 {
     // Accessor for Option contract details
     mapping(uint256 => Option) public option;
 
+    mapping(bytes32 => bool) chainMap;
+
     // Accessor for claim ticket details
     mapping(uint256 => Claim) public claim;
 
@@ -63,7 +75,47 @@ contract OptionSettlementEngine is ERC1155 {
     function uri(uint256) public pure virtual override returns (string memory) {
         // https://eips.ethereum.org/EIPS/eip-1155#metadata
         // Return base64 encoded json blob with metadata for rendering on the frontend
-        return "";
+        //{
+        //    "title": "Token Metadata",
+        //"type": "object",
+        //"properties": {
+        //"name": {
+        //"type": "string",
+        //"description": "Identifies the asset to which this token represents"
+        //},
+        //"decimals": {
+        //"type": "integer",
+        //"description": "The number of decimal places that the token amount should display - e.g. 18, means to divide the token amount by 1000000000000000000 to get its user representation."
+        //},
+        //"description": {
+        //"type": "string",
+        //"description": "Describes the asset to which this token represents"
+        //},
+        //"image": {
+        //"type": "string",
+        //"description": "A URI pointing to a resource with mime type image/* representing the asset to which this token represents. Consider making any images at a width between 320 and 1080 pixels and aspect ratio between 1.91:1 and 4:5 inclusive."
+        //},
+        //"properties": {
+        //"type": "object",
+        //"description": "Arbitrary properties. Values may be strings, numbers, object or arrays."
+        //}
+        //}
+        //    }
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked("{}")
+                )
+            )
+        );
+        return string(abi.encodePacked("data:application/json;base64,", json));
+    }
+
+    // TODO(random number should be generated from vrf)
+    function newOptionsChain(Option memory option) {
+        // TODO(This should check that a duplicate chain doesn't exist, and iff it does, revert)
+        // Else, create new options chain
+        // TODO(There should be at least 24 hours between expiry and exercise)
     }
 
     // Here there must exist a way to get the data about a given token id transparently

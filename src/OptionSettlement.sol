51947a8adfa2e24bb8dc1467b8b877e1105e0690
Alcibiades Athens <alcibiades.eth@protonmail.com>
2022-03-20 11:47:00 -0400

    add transfer for options writing

--- src/OptionSettlement.sol
+++ src/OptionSettlement.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.11;
 import "base64/Base64.sol";
 import "solmate/tokens/ERC20.sol";
 import "solmate/tokens/ERC1155.sol";
+import "solmate/utils/SafeTransferLib.sol";
 
 /**
    Valorem Options V1 is a DeFi money lego enabling writing covered call and covered put, physically settled, options.
@@ -59,6 +60,8 @@ contract OptionSettlementEngine is ERC1155 {
 
     // TODO(Events for subgraph)
 
+    // TODO(Do we need to track internal balances)
+
     // To increment the next available token id
     uint256 private _nextTokenId;
 
@@ -84,41 +87,13 @@ contract OptionSettlementEngine is ERC1155 {
         returns (string memory)
     {
         require(tokenType[tokenId] != Type.None, "Token does not exist");
-        // https://eips.ethereum.org/EIPS/eip-1155#metadata
-        // Return base64 encoded json blob with metadata for rendering on the frontend
-        //{
-        //    "title": "Token Metadata",
-        //"type": "object",
-        //"properties": {
-        //"name": {
-        //"type": "string",
-        //"description": "Identifies the asset to which this token represents"
-        //},
-        //"decimals": {
-        //"type": "integer",
-        //"description": "The number of decimal places that the token amount should display - e.g. 18, means to divide the token amount by 1000000000000000000 to get its user representation."
-        //},
-        //"description": {
-        //"type": "string",
-        //"description": "Describes the asset to which this token represents"
-        //},
-        //"image": {
-        //"type": "string",
-        //"description": "A URI pointing to a resource with mime type image/* representing the asset to which this token represents. Consider making any images at a width between 320 and 1080 pixels and aspect ratio between 1.91:1 and 4:5 inclusive."
-        //},
-        //"properties": {
-        //"type": "object",
-        //"description": "Arbitrary properties. Values may be strings, numbers, object or arrays."
-        //}
-        //}
-        //    }
+        // TODO(Implement metadata/uri builder)
         string memory json = Base64.encode(
             bytes(string(abi.encodePacked("{}")))
         );
         return string(abi.encodePacked("data:application/json;base64,", json));
     }
 
-    // TODO(random number should be generated from vrf)
     function newOptionsChain(Option memory optionInfo)
         external
         returns (uint256 tokenId)
@@ -145,6 +120,9 @@ contract OptionSettlementEngine is ERC1155 {
 
         // Zero out random number for gas savings and to await randomness
         optionInfo.settlementSeed = 0;
+
+        // TODO(random number should be generated from vrf and stored to settlementSeed here)
+        optionInfo.settlementSeed = 42;
 
         // Create option token and increment
         tokenType[_nextTokenId] = Type.Option;
@@ -174,10 +152,19 @@ contract OptionSettlementEngine is ERC1155 {
         chainMap[chainKey] = true;
     }
 
-    function writeOption(uint256 tokenId) external view {
+    function mintClaim() internal {}
+
+    function writeOption(uint256 tokenId, uint256 amount) external {
         require(tokenType[tokenId] == Type.Option, "Token is not an option");
 
-        // TODO(Transfer the requisite underlying asset)
+        // Transfer the requisite underlying asset
+        SafeTransferLib.safeTransferFrom(
+            ERC20(option[tokenId].underlyingAsset),
+            msg.sender,
+            address(this),
+            (amount * option[tokenId].underlyingAmount)
+        );
+
         // TODO(Create and transfer the claim token)
         // TODO(Emit event about the writing)
     }

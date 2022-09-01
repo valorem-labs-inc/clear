adef4eaafef5acd2a46ded65c3328091da52063a
thal0x <thal0x.eth@gmail.com>
2022-09-01 14:10:00 -0500

    implement VolatilityOracle

--- src/test/Volatility.t.sol
+++ src/test/Volatility.t.sol
@@ -11,12 +11,31 @@ import "../libraries/UniV3Oracle.sol";
 import "../VolatilityOracle.sol";
 
 contract VolatilityTest is Test {
-    mapping(IUniswapV3Pool => Volatility.FeeGrowthGlobals[25])
-        public feeGrowthGlobals;
-
-    function testSomething() public {
+    function testCachePoolMetadata() public {
         IUniswapV3Pool pool = IUniswapV3Pool(
-            0x5777d92f208679DB4b9778590Fa3CAB3aC9e2168
+            0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8
+        );
+
+        VolatilityOracle oracle = new VolatilityOracle();
+
+        oracle.cachePoolMetadata(pool);
+
+        (
+            uint32 maxSecondsAgo,
+            uint24 gamma0,
+            uint24 gamma1,
+            int24 tickSpacing
+        ) = oracle.cachedPoolMetadata(pool);
+
+        assertEq(maxSecondsAgo, 138487);
+        assertEq(gamma0, 3000);
+        assertEq(gamma1, 3000);
+        assertEq(tickSpacing, 60);
+    }
+
+    function testVolatilityForPool() public {
+        IUniswapV3Pool pool = IUniswapV3Pool(
+            0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8
         );
 
         VolatilityOracle oracle = new VolatilityOracle();
@@ -24,9 +43,15 @@ contract VolatilityTest is Test {
         oracle.cachePoolMetadata(pool);
 
         uint256 iv = oracle.volatilityForPool(pool);
+        assertEq(iv, 8329695708861862);
 
-        console.log(iv);
-
-        assertTrue(true);
+        (
+            uint256 feeGrowthGlobal0,
+            uint256 feeGrowthGlobal1,
+            uint256 timestamp
+        ) = oracle.feeGrowthGlobals(pool, 1);
+        assertEq(feeGrowthGlobal0, 2632765082876750956909549161655207);
+        assertEq(feeGrowthGlobal1, 1138771352674128276125939260909996260194545);
+        assertEq(timestamp, 1661876584);
     }
 }

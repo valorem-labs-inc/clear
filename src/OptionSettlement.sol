848286b5859469d36817eb0ae6d62bdc9fa86969
Flip Liquid <13227294+Flip-Liquid@users.noreply.github.com>
2022-10-06 11:16:26 -0400

    correct contract comment

--- src/OptionSettlement.sol
+++ src/OptionSettlement.sol
@@ -11,7 +11,7 @@ import "./TokenURIGenerator.sol";
 /**
  * Valorem Options V1 is a DeFi money lego enabling writing covered call and covered put, physically settled, options.
  * All written options are fully collateralized against an ERC-20 underlying asset and exercised with an
- * ERC-20 exercise asset using a chainlink VRF random number per unique option type for fair settlement. Options contracts
+ * ERC-20 exercise asset using a pseudorandom number per unique option type for fair settlement. Options contracts
  * are issued as fungible ERC-1155 tokens, with each token representing a contract. Option writers are additionally issued
  * an ERC-1155 NFT representing a lot of contracts written for claiming collateral and exercise assignment. This design
  * eliminates the need for market price oracles, and allows for permission-less writing, and gas efficient transfer, of

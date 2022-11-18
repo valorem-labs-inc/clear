200e748073a208a8700b27ee8cbfb51bd18c8156
neodaoist <tom@loudverse.com>
2022-11-18 12:12:19 -0500

    Pin pragma for slither

--- src/interfaces/IERC1155.sol
+++ src/interfaces/IERC1155.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts (last updated v4.7.0) (token/ERC1155/IERC1155.sol)
 
-pragma solidity ^0.8.0;
+pragma solidity 0.8.11;
 
 import "./IERC165.sol";
 

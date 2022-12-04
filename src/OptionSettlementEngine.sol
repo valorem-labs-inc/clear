d07d9d454d0e32455a9d9d277728b2b25adcf8a3
Flip Liquid <13227294+Flip-Liquid@users.noreply.github.com>
2022-12-03 19:21:17 -0500

    Revert "fix loss of precision in fee"
    This reverts commit a230354d6ae2dcc223e10f39a33f7bcab8b88142.

--- src/OptionSettlementEngine.sol
+++ src/OptionSettlementEngine.sol
@@ -306,7 +306,7 @@ contract OptionSettlementEngine is ERC1155, IOptionSettlementEngine {
 
         // Calculate the amount of underlying and exercise assets to transfer
         uint256 rxAmount = amount * optionRecord.underlyingAmount;
-        uint256 fee = ((rxAmount * feeBps) / 10_000);
+        uint256 fee = ((rxAmount / 10_000) * feeBps);
         address underlyingAsset = optionRecord.underlyingAsset;
 
         feeBalance[underlyingAsset] += fee;

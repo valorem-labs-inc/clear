87d32898c5a4db1aa709be7d96ce8664d826f75c
Alcibiades Athens <alcibiades.eth@protonmail.com>
2022-12-10 00:04:48 -0500

    fix credit in main contract

--- src/OptionSettlementEngine.sol
+++ src/OptionSettlementEngine.sol
@@ -25,7 +25,6 @@ import "./TokenURIGenerator.sol";
 /// @author 0xAlcibiades
 /// @author Flip-Liquid
 /// @author neodaoist
-/// @author Thal0x
 contract OptionSettlementEngine is ERC1155, IOptionSettlementEngine {
     /*//////////////////////////////////////////////////////////////
     //  Immutable/Constant - Private

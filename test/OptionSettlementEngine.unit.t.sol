17d296ec7d020cf888bd574fd6bc4cddd9f9b5cb
Alcibiades Athens <alcibiades.eth@protonmail.com>
2022-12-22 00:33:05 -0500

    fix headers in unit test file

--- test/OptionSettlementEngine.unit.t.sol
+++ test/OptionSettlementEngine.unit.t.sol
@@ -8,9 +8,10 @@ import {IERC20} from "forge-std/interfaces/IERC20.sol";
 import "./utils/BaseEngineTest.sol";
 
 contract OptionSettlementUnitTest is BaseEngineTest {
-    //
-    // function option(uint256 tokenId) external view returns (Option memory optionInfo);
-    //
+    /*//////////////////////////////////////////////////////////////
+    //  function option(uint256 tokenId) external view returns (Option memory optionInfo);
+    //////////////////////////////////////////////////////////////*/
+
     function test_unitOptionRevertsWhenDoesNotExist() public {
         uint256 badOptionId = 123;
         vm.expectRevert(abi.encodeWithSelector(IOptionSettlementEngine.TokenNotFound.selector, badOptionId));
@@ -27,9 +28,10 @@ contract OptionSettlementUnitTest is BaseEngineTest {
         assertEq(optionInfo.expiryTimestamp, testExpiryTimestamp);
     }
 
-    //
-    // function claim(uint256 claimId) external view returns (Claim memory claimInfo);
-    //
+    /*//////////////////////////////////////////////////////////////
+    //  function claim(uint256 claimId) external view returns (Claim memory claimInfo);
+    //////////////////////////////////////////////////////////////*/
+
     function test_unitClaimRevertsWhenClaimDoesNotExist() public {
         uint256 badClaimId = testOptionId + 69;
         vm.expectRevert(abi.encodeWithSelector(IOptionSettlementEngine.TokenNotFound.selector, badClaimId));
@@ -161,9 +163,10 @@ contract OptionSettlementUnitTest is BaseEngineTest {
         );
     }
 
-    //
-    // function position(uint256 tokenId) external view returns (Position memory positionInfo);
-    //
+    /*//////////////////////////////////////////////////////////////
+    //  function position(uint256 tokenId) external view returns (Position memory positionInfo);
+    //////////////////////////////////////////////////////////////*/
+
     function test_unitPositionRevertsTokenNotFound() public {
         vm.expectRevert(abi.encodeWithSelector(IOptionSettlementEngine.TokenNotFound.selector, 1));
         engine.position(1);
@@ -232,9 +235,10 @@ contract OptionSettlementUnitTest is BaseEngineTest {
         assertEq(position.exerciseAmount, int256(uint256(amountWritten * testUnderlyingAmount)));
     }
 
-    //
-    // function tokenType(uint256 tokenId) external view returns (TokenType typeOfToken);
-    //
+    /*//////////////////////////////////////////////////////////////
+    //  function tokenType(uint256 tokenId) external view returns (TokenType typeOfToken);
+    //////////////////////////////////////////////////////////////*/
+
     function test_unitTokenTypeReturnsNone() public view {
         assert(IOptionSettlementEngine.TokenType.None == engine.tokenType(127));
     }
@@ -250,30 +254,31 @@ contract OptionSettlementUnitTest is BaseEngineTest {
         assert(IOptionSettlementEngine.TokenType.Claim == engine.tokenType(claimId));
     }
 
-    //
-    // function tokenURIGenerator() external view returns (ITokenURIGenerator uriGenerator);
-    //
+    /*//////////////////////////////////////////////////////////////
+    //  function tokenURIGenerator() external view returns (ITokenURIGenerator uriGenerator);
+    //////////////////////////////////////////////////////////////*/
+
     function test_unitTokenURIGenerator() public view {
         assert(address(engine.tokenURIGenerator()) == address(generator));
     }
 
-    //
-    // function feeBalance(address token) external view returns (uint256);
-    //
+    /*//////////////////////////////////////////////////////////////
+    //  function feeBalance(address token) external view returns (uint256);
+    //////////////////////////////////////////////////////////////*/
 
-    //
-    // function feeBps() external view returns (uint8 fee);
-    //
+    /*//////////////////////////////////////////////////////////////
+    //  function feeBps() external view returns (uint8 fee);
+    //////////////////////////////////////////////////////////////*/
 
-    //
-    // function feesEnabled() external view returns (bool enabled);
-    //
+    /*//////////////////////////////////////////////////////////////
+    //  function feesEnabled() external view returns (bool enabled);
+    //////////////////////////////////////////////////////////////*/
 
-    //
-    // function feeTo() external view returns (address);
-    //
+    /*//////////////////////////////////////////////////////////////
+    //  function feeTo() external view returns (address);
+    //////////////////////////////////////////////////////////////*/
 
-    //
+    /*//////////////////////////////////////////////////////////////
     // function newOptionType(
     //        address underlyingAsset,
     //        uint96 underlyingAmount,
@@ -282,32 +287,35 @@ contract OptionSettlementUnitTest is BaseEngineTest {
     //        uint40 exerciseTimestamp,
     //        uint40 expiryTimestamp
     //    ) external returns (uint256 optionId);
-    //
+    //////////////////////////////////////////////////////////////*/
 
-    //
+    /*//////////////////////////////////////////////////////////////
     // function write(uint256 tokenId, uint112 amount) external returns (uint256 claimId);
-    //
+    //////////////////////////////////////////////////////////////*/
 
-    //
+    /*//////////////////////////////////////////////////////////////
     // function redeem(uint256 claimId) external;
-    //
+    //////////////////////////////////////////////////////////////*/
 
-    //
+    /*//////////////////////////////////////////////////////////////
     // function exercise(uint256 optionId, uint112 amount) external;
+    //////////////////////////////////////////////////////////////*/
 
-    //
+    /*//////////////////////////////////////////////////////////////
     // function setFeesEnabled(bool enabled) external;
+    //////////////////////////////////////////////////////////////*/
 
-    //
+    /*//////////////////////////////////////////////////////////////
     // function setFeeTo(address newFeeTo) external;
+    //////////////////////////////////////////////////////////////*/
 
-    //
+    /*//////////////////////////////////////////////////////////////
     // function setTokenURIGenerator(address newTokenURIGenerator) external;
-    //
+    //////////////////////////////////////////////////////////////*/
 
-    //
+    /*//////////////////////////////////////////////////////////////
     // function sweepFees(address[] memory tokens) external;
-    //
+    //////////////////////////////////////////////////////////////*/
 
     // TODO(Categorize and dedup/audit tests below this line)
 

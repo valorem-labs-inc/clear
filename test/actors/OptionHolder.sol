1065410f031b3d28633fc56d967d78c83082b5ba
Flip <13227294+Flip-Liquid@users.noreply.github.com>
2022-12-23 17:15:49 -0600

    exercise behavior for actor

--- test/actors/OptionHolder.sol
+++ test/actors/OptionHolder.sol
@@ -10,5 +10,14 @@ contract OptionHolder is BaseActor {
 
     function exercise() external {
         console.logString("exercise");
+        uint256[] memory optionTypes = test.getOptionTypes();
+        // Retrieve all option types
+        for (uint256 i = 0; i < optionTypes.length; i++) {
+            uint256 optionType = optionTypes[i];
+            uint256 optionBalance = engine.balanceOf(address(this), optionType);
+            // exercise a random amount up to the total balance
+            uint256 toExercise = _randBetween(uint32(block.timestamp), optionBalance);
+            engine.exercise(optionType, uint112(toExercise));
+        }
     }
 }
